c3b6d0b26b2bea003f648c0d69926b7224f44654
msierra <65048229+msierras@users.noreply.github.com>
2026-04-10 19:17:41 -0400

    Add: string-encode-and-decode - submission-34

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-34.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string res = "";
+        for(auto str : strs){
+            res += to_string(str.size()) + "#" + str;
+        }
+        return res;
+    }
+
+    // 5#Hello5#World
+
+    vector<string> decode(string s) {
+        vector<string> res;
+
+        int i = 0;
+        int j;
+
+        while( i < s.size() ){
+            j = i;
+
+
+            while(s[j] != '#'){
+                j++;
+            }
+            
+            int len = stoi(s.substr(i, j));
+
+            res.push_back(s.substr(j + 1, len));
+
+            // Without the +1, in the next iteration, i will start at a
+            // character instead of a number
+            i = j + len + 1;
+        }
+
+        return res;
+    }
+};
