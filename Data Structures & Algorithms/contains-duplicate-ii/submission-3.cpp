e26934216655cf410922dfafef0e90a717daa934
msierra <65048229+msierras@users.noreply.github.com>
2026-06-09 19:37:41 -0400

    Add: contains-duplicate-ii - submission-3

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-3.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        
+        int left = 0;
+        unordered_set<int> set;
+
+        for(int right = 0; right < nums.size(); right++){
+
+            if( right - left  > k ){
+                set.erase(nums[left]);
+                left++;
+            }
+
+            if( set.contains( nums[right] ) ){
+                return true;
+            }
+
+            set.insert( nums[right] );
+        }
+
+        return false;
+
+    }
+};
