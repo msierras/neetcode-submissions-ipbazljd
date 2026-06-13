d6f51b63ffbdb110956c010abaebb29665bebb87
msierra <65048229+msierras@users.noreply.github.com>
2026-06-13 13:39:20 -0400

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+        int left = 0;
+        int right = 1;
+
+        int res = 0;
+
+        while( right < prices.size() ){
+
+            if( prices[left] < prices[right] ){
+                res = max(prices[right] - prices[left], res);
+            }
+            else{
+                left = right;
+            }
+
+            right++;
+        }
+
+        return res;
+    }
+};
