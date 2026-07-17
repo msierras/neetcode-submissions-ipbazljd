3eec3eea9a69973b6d4b2ef0bd4e7308118bb458
msierra <65048229+msierras@users.noreply.github.com>
2026-07-17 19:55:43 -0400

    Add: baseball-game - submission-2

---
+++ Data Structures & Algorithms/baseball-game/submission-2.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        vector<int> stck;
+
+
+        for(string &operation : operations){
+ 
+            if( operation == "+" ){
+                stck.push_back( stck[stck.size()-1] + stck[stck.size()-2] );
+            }
+            else if( operation == "D" ){
+                stck.push_back( stck[stck.size() - 1] * 2 );
+            }
+            else if( operation == "C" ){
+                stck.pop_back();
+            }
+            else{   // When 
+                stck.push_back( std::stoi(operation) );
+            }
+
+
+        }
+
+        int res = 0;
+        for(auto &element : stck){
+            res += element;
+        }
+
+        return res;
+
+    }
+};
