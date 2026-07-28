b03844e1a0d5df160a967ad19f5adc73c05f8f37
msierra <65048229+msierras@users.noreply.github.com>
2026-07-27 20:03:13 -0400

    Add: validate-parentheses - submission-11

---
+++ Data Structures & Algorithms/validate-parentheses/submission-11.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isValid(string s) {
+
+        if(s.size() <= 1) return false;
+        
+        stack<char> open;
+
+        for(const auto &currChar : s){
+
+            if( currChar == '(' || currChar == '{' || currChar == '['  ){
+                open.push(currChar);
+            }
+            else if( currChar == ')' || currChar == '}' || currChar == ']' ){
+                if( !open.empty() && currChar == ')' && open.top() == '(' ) open.pop();
+                else if( !open.empty() && currChar == '}' && open.top() == '{' ) open.pop();
+                else if( !open.empty() && currChar == ']' && open.top() == '[' ) open.pop();
+                else return false;
+
+            }
+
+        }
+
+        // Should be empty if is a valid parentheses
+        return open.empty();
+
+    }
+};
