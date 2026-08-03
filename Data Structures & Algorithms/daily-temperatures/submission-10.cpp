e1b797f80cc08cedd63b09f2ddb33493007f24c5
msierra <65048229+msierras@users.noreply.github.com>
2026-08-03 19:37:17 -0400

    Add: daily-temperatures - submission-10

---
+++ Data Structures & Algorithms/daily-temperatures/submission-10.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        
+        vector<int> res( temperatures.size(), 0 );
+        stack<int> indices; 
+
+        for(int i = 0; i < temperatures.size(); i++){
+
+            int temp = temperatures[i];
+
+            while( !indices.empty() &&  temp > temperatures[indices.top()] ){
+                res[indices.top()] = i - indices.top();
+                indices.pop();
+            }          
+
+            indices.push(i);
+        }
+
+        return res;
+    }
+};
